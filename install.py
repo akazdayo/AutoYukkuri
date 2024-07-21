8f09910c39c051bc3d47d41aa3e1af8a4c6f01c6
akazdayo <82073147+akazdayo@users.noreply.github.com>
2024-07-21 14:49:40 +0900

    GPU Torchのインストール(バグあり)

--- install.py
+++ install.py
@@ -4,6 +4,7 @@ import subprocess
 import sys
 import importlib.util
 import re
+import argparse
 
 
 def install_pip_package(package):
@@ -34,6 +35,7 @@ def get_pip_requirements():
             version_numbers.append(match.group(2))
             yield match.group(1), match.group(2)
 
+
 def pip_install():
     for package, version in get_pip_requirements():
         print(package, str(version))
@@ -41,16 +43,36 @@ def pip_install():
             print(f"{package} is not installed. Installing...")
             install_pip_package(f"{package}=={version}")
 
+
 def check_other_install():
     try:
         subprocess.check_call(["ffmpeg", "-version"])
     except FileNotFoundError:
         print("FFmpegがインストールされていません。インストールしてください。")
         sys.exit(1)
+    if args.gpu:
+        try:
+            output = subprocess.check_output(["nvcc", "--version"])
+            lines = output.decode("utf-8").splitlines()
+            cuda_version_line = lines[3]
+            cuda_version = cuda_version_line.split(", ")[1].split(" ")[1]
+            print(f"CUDA version: {cuda_version}")
+            if cuda_version != "11.8" or cuda_version != "12.1":
+                print("CUDAのバージョンが11.8または12.1ではありません。\n11.8または12.1をインストールしてください。")
+                sys.exit(1)
+
+            install_pip_package(f"torch --index-url https://download.pytorch.org/whl/cu{cuda_version.replace(".","")}")
+        except FileNotFoundError:
+            print("CUDAがインストールされていません。バージョン11.8または12.1をインストールしてください。")
+            sys.exit(1)
     print("全てのパッケージがインストールされています。")
 
 
-
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gpu', action='store_true',
+                        help='Use GPU acceleration')
+    args = parser.parse_args()
+
     pip_install()
     check_other_install()
